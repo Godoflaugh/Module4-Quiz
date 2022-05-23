24899aa74c3160252f9185b2cc8940db383193c3
Godoflaugh <lam.kevin91@gmail.com>
2022-05-23 13:28:59 -0700

    Initiliaze the repository with base file projects. Added index.html, JS, and CSS at base level.

---
+++ assets/js/javascript.js
@@ -0,0 +1,11 @@
+var startBtn = document.querySelector("#start")
+startBtn.addEventListener("click", startQuiz)
+
+
+// This will start the quiz, beginnging the timer and transitioning to the questions.
+function startQuiz() {
+
+  if(startBtn === true)
+console.log("Helllo World")
+}
+
