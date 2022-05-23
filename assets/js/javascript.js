d2035f7d7aa8a54619f91a801756dd61143f1037
Godoflaugh <lam.kevin91@gmail.com>
2022-05-23 15:39:56 -0700

    Added timer functionality, bug with timer starting without user event

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,48 @@
+// Variable declaration
+
 var startBtn = document.querySelector("#start")
-startBtn.addEventListener("click", startQuiz)
+var countDownEL = document.querySelector("#countDown")
+
+// creates the timer to be 1 second
+// var seconds = 1000 * 60
+// creates variable to represent 1 minute
+// var timer = seconds * 60
+
+
+
+// Timer Function
+function cdTimer() {
+  var timeLeft = 90
+
+  // Listen's for click 
+  startBtn.addEventListener("click", startQuiz)
+
+  var timeInterval = setInterval(function () {
+
+    // Checks to see if time is greater than 1
+    if (timeLeft > 1) {
+      countDownEL.textContent = timeLeft + " seconds remaining"
+      timeLeft--
+      console.log(timeLeft)
+    } else if (timeLeft === 1) {
+      countDownEL.textContent = timeLeft + " second remaining"
+      timeLeft--
+    } else {
+      // once timer reaches 0 then we clear the time counter
+      countDownEL.textContent = ""
+      // this will stop the timer
+      clearInterval(timeLeft)
+      // call to a function to execute, this should be a function that resets the code back to zero
+    }
+  }, 1000)
+
+}
 
 
 // This will start the quiz, beginnging the timer and transitioning to the questions.
 function startQuiz() {
-
-  if(startBtn === true)
-console.log("Helllo World")
+  cdTimer()
 }
 
+
+startQuiz()
